a11bddac221a20e2c4113aaa51eaf42f4142274f
Iniubong Arthur <arthurshur@gmail.com>
2021-03-05 01:53:06 +0100

    modified

--- chapterTwo/NegativePositiveNegativeZero.java
+++ chapterTwo/NegativePositiveNegativeZero.java
@@ -16,5 +16,53 @@ public class NegativePositiveNegativeZero {
         int number5 = scanner.nextInt();
 
 
+        if(number1 > 0){
+            System.out.println(number1 + " is a positive number");
+        }
+        if(number2 > 0){
+            System.out.println(number2 + " is a positive number");
+        }
+        if(number3 > 0){
+            System.out.println(number3 + " is a positive number");
+        }
+        if(number4 > 0){
+            System.out.println(number4 + " is a positive number");
+        }
+        if(number5 > 0){
+            System.out.println(number5 + " is a positive number");
+        }
+
+        if(number1 < 0){
+            System.out.println(number1 + " is a negative number");
+        }
+        if(number2 < 0){
+            System.out.println(number2 + " is a negative number");
+        }
+        if(number3 < 0){
+            System.out.println(number3 + " is a negative number");
+        }
+        if(number4 < 0){
+            System.out.println(number4 + " is a negative number");
+        }
+        if(number5 < 0){
+            System.out.println(number5 + " is a negative number");
+        }
+
+        if(number1 == 0){
+            System.out.println(number1 + " is zero ");
+        }
+        if(number2 == 0){
+            System.out.println(number2 + " is zero ");
+        }
+        if(number3 == 0){
+            System.out.println(number3 + " is zero ");
+        }
+        if(number4 == 0){
+            System.out.println(number4 + " is zero ");
+        }
+        if(number5 == 0){
+            System.out.println(number5 + " is zero ");
+        }
+
     }
 }
